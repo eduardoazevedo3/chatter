5e71119f7beb46963d66b00babf2923f33c3921b
Eduardo Azevedo <eduardoazevedo3@gmail.com>
2021-12-15 17:58:51 -0300

    refactor(api): remove puts in chat message channel

--- api/app/channels/chat_messages_channel.rb
+++ api/app/channels/chat_messages_channel.rb
@@ -1,8 +1,5 @@
 class ChatMessagesChannel < ApplicationCable::Channel
   def subscribed
-    p '***'
-    p params
-    p '---'
     stream_from "chat_messages_#{current_user.id}" if current_user
   end
 
